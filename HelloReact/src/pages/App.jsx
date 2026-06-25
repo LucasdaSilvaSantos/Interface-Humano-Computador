a99b73673658fe6ebe755ae2f0e2fc4e0aa15bc0
Lucas da Silva Santos <ldsantos2004@gmail.com>
2026-06-25 19:10:47 -0300

    Atualizando o projeto_2

---
+++ HelloReact/src/pages/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Perfil from '../components/Perfil'
+import Saudacao from '../components/Saudacao'
+import Contador from '../components/Contador'
+import Feed from '../components/Feed'
+import NovoPost from '../components/NovoPost'
+
+const App = () => {
+  return (
+    <div>
+      <NovoPost />
+    </div>
+  )
+}
+
+export default App
